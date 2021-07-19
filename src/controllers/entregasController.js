35325129330e90db8faf03d925b9a2122ad8b611
amandazepechouka <aamzepechouka@gmail.com>
2021-07-19 00:03:55 -0300

    [fix] Corrige o relacionamento de entrega com associados, clientes e motoboys

--- src/controllers/entregasController.js
+++ src/controllers/entregasController.js
@@ -8,66 +8,66 @@ module.exports = {
   async newEntrega(req, res) {
     const { description, clienteId, motoboyId } = req.body;
     if (!description || !clienteId || !motoboyId) {
-        res.status(400).json({ msg: "Dados obrigatórios não foram preenchidos." });
+      res.status(400).json({ msg: "Dados obrigatórios não foram preenchidos." });
     }
 
     const isEntregaNew = await isEntregaNew.findOne({
-        where: { description },
+      where: { description },
     });
-    
-    if(isEntregaNew) 
-        res.status(403).json( { msg: "Entrega já foi cadastrada."});
+
+    if (isEntregaNew)
+      res.status(403).json({ msg: "Entrega já foi cadastrada." });
     else {
-        const entrega = await Entregas.create({
-            description,
-            clienteId,
-            motoboyId,
-        }).catch((error) => {
-            res.status(500).json({ msg: "Não foi possível inserir os dados."});
-        });
-        if (entrega)
-            res.status(201).json({ msg: "Nova entrega foi adicionada."});
-        else
-            res.status(404).json({ msg: "Não foi possível cadastrar nova entrega." });
+      const entrega = await Entregas.create({
+        description,
+        clienteId,
+        motoboyId,
+      }).catch((error) => {
+        res.status(500).json({ msg: "Não foi possível inserir os dados." });
+      });
+      if (entrega)
+        res.status(201).json({ msg: "Nova entrega foi adicionada." });
+      else
+        res.status(404).json({ msg: "Não foi possível cadastrar nova entrega." });
     }
   },
 
   async listAllEntrega(req, res) {
     const entrega = await Entregas.findAll({
-      order: [["entregaId", "ASC"]],
+      order: [["id", "ASC"]],
     }).catch((error) => {
       res.status(500).json({ msg: "Falha na conexão." });
     });
-    if (entrega) 
-        res.status(200).json({ entrega });
-    else 
-        res.status(404).json({ msg: "Não foi possível encontrar entregas." });
+    if (entrega)
+      res.status(200).json({ entrega });
+    else
+      res.status(404).json({ msg: "Não foi possível encontrar entregas." });
   },
 
   async listEntregaReal(req, res) {
     const entrega = await Entregas.findAll({
-        order: [[ "status" ]],
+      order: [["status"]],
     });
     if (entrega) {
-        if (entrega == "Realizado")
-            res.status(404).json({ msg: "Entrega não encontrada" });
-        else 
-            res.status(200).json({ entrega });
+      if (entrega == "Realizado")
+        res.status(404).json({ msg: "Entrega não encontrada" });
+      else
+        res.status(200).json({ entrega });
     } else
-        res.status(404).json({ msg: "Entrega não encontrada." });
+      res.status(404).json({ msg: "Entrega não encontrada." });
   },
 
   async listEntregaPend(req, res) {
     const entrega = await Entregas.findAll({
-        order: [[ "status" ]],
+      order: [["status"]],
     });
     if (entrega) {
-        if (entrega == "Pendente")
-            res.status(404).json({ msg: "Entrega não encontrada" });
-        else 
-            res.status(200).json({ entrega });
+      if (entrega == "Pendente")
+        res.status(404).json({ msg: "Entrega não encontrada" });
+      else
+        res.status(200).json({ entrega });
     } else
-        res.status(404).json({ msg: "Entrega não encontrada." });
+      res.status(404).json({ msg: "Entrega não encontrada." });
   },
 
   async searchEntregaByMotoboy(req, res) {
@@ -76,27 +76,27 @@ module.exports = {
       res.status(400).json({ msg: "Campo vendedor vazio." });
     const entregas = await Motoboys.findAll({
       where: { motoboyId },
-    }).catch((error) => 
-        res.status(500).json({ msg: "Falha na conexão." }));
+    }).catch((error) =>
+      res.status(500).json({ msg: "Falha na conexão." }));
     if (entregas) {
       if (entregas == "")
         res.status(404).json({ msg: "Não há vendas para este motoboy." });
-      else 
+      else
         res.status(200).json({ entregas });
-    } else 
-        res.status(404).json({ msg: "Não foi possível encontrar entregas." });
+    } else
+      res.status(404).json({ msg: "Não foi possível encontrar entregas." });
   },
 
-  async updateEntregaPend(req, res) {},
+  async updateEntregaPend(req, res) { },
 
   async deleteEntrega(req, res) {
     const entregaId = req.params.id;
     const deleteEntrega = await Entregas.destroy({
-        where: { id: entregaId },
+      where: { id: entregaId },
     });
     if (deleteEntrega != 0)
-        res.status(200).json({ msg: "Entrega excluido com sucesso." });
+      res.status(200).json({ msg: "Entrega excluido com sucesso." });
     else
-        res.status(404).json({ msg: "Entrega não encontrado." });
+      res.status(404).json({ msg: "Entrega não encontrado." });
   },
 };
